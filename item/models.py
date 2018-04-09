8bb0795337cf9aac0ab9c2dfd355cea7a0f87351
RomChung <thelns@vip.qq.com>
2018-04-10 01:16:21 +0800

    czj是牛逼的

--- item/models.py
+++ item/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import  User
+# Create your models here.
 
 
 class ItemType(models.Model):
@@ -9,16 +8,26 @@ class ItemType(models.Model):
 
     def __str__(self):
         return self.type_name
-# 定义商品类型
 
+
+# 定义商品类型
 class Item(models.Model):
-    title = models.CharField (max_length = 30)#商品名称
-    content = models.TextField()#商品内容
+
+    # 商品名称
+    title = models.CharField (max_length = 30)
+    # 商品内容
+    content = models.TextField()
+    #商品类型
     item_type = models.ForeignKey (ItemType,on_delete= models.DO_NOTHING)
-    author  = models.ForeignKey (User,on_delete=models.DO_NOTHING) #商品主人
-    created_time = models.DateTimeField (auto_now_add= True)#发布商品的时间
+    # 商品主人
+    author  = models.ForeignKey (User,on_delete=models.DO_NOTHING)
+    # 发布商品的时间
+    created_time = models.DateTimeField (auto_now_add= True)
+    #最后更新时间
     last_updated_time = models.DateTimeField (auto_now= True)
-    price = models.FloatField(default = 0)#商品价格 初始为0
+    # 商品价格 初始为0
+    price = models.FloatField(default = 0)
+
 
     def  __str__(self):
         return '<Item: %s>'%self.title
--- item/urls.py
+++ item/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('<int:item_pk>',views.item_detail,name = 'item_detail'),
     #localhost:8000/item/(商品编号)
     path('list/',views.item_list,name = 'item_list'),
-    #localhost：8000/itesm/list
-    path("type/<int:item_type_pk>",views.items_with_type,name = 'items_with_type'),
+    #localhost：8000/item/list
+    path("type/<int:items_with_type_pk>",views.items_with_type,name = 'items_with_type'),
 ]
