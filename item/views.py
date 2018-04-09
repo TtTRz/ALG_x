03c5b2478d72423cc7109ff25dd37bb26e4898b5
czj <1143144598@qq.com>
2018-04-09 23:36:51 +0800

    加了个商品分类，路由有点问题

--- item/views.py
+++ item/views.py
@@ -4,12 +4,21 @@ from . models import Item,ItemType
 
 # 商品列表
 def item_list(request):
-    content = {}
-    content['items'] = Item.objects.all()#返回所有商品的信息
+    context = {}
+    context['items'] = Item.objects.all()#返回所有商品的信息
 
-    return render_to_response('item/item_list.html',content)
+    return render_to_response('item/item_list.html',context)
 # 商品详细内容
-def item_detail(request,item_pk):
-    content = {}
-    content ['item'] = get_object_or_404(Item, pk =item_pk) #如果没有该商品目录则返回404
-    return render_to_response('item/item_detail.html',content)
+def  item_detail(request,item_pk):
+    context = {}
+    context ['item'] = get_object_or_404(Item, pk =item_pk) #如果没有该商品目录则返回404
+    return render_to_response('item/item_detail.html',context)
+
+
+#商品类型分类（同一类放在一起）
+def  items_with_type(request,items_with_type):
+    context = {}
+    item_type = get_object_or_404(ItemType,pk = items_with_type)
+    context['items'] = Item.objects.filter(item_type = item_type)
+    context['item_type'] = item_type
+    return render_to_response('item/items_with_type.html',context)
