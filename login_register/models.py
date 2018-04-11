2b0d98fb52242d67230160c885fa55b428a981e9
121 <>
2018-04-11 08:44:44 +0800

    修改用户管理系统bug， 增加取消登陆功能

--- login_register/models.py
+++ login_register/models.py
@@ -2,30 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Person(models.Model):
-    # 用户名
-    username = models.CharField('用户名', max_length=30, default='')
-    # 用户密码
-    password = models.CharField('密码', max_length=30, default='')
-    # 用户年龄
-    age = models.IntegerField('年龄', default=0, null=True,blank=True)
-    # 用户角色
-    role = models.ForeignKey('User_Role', on_delete=models.CASCADE, null=True, blank=True)
-    # 用户所处年级
-    grade = models.ForeignKey('User_Grade', on_delete=models.CASCADE, null=True, blank=True)
-    # 用户真实名称
-    name = models.CharField('真实姓名', max_length=30, default='', null=True)
-    # 联系电话
-    telephone = models.CharField('联系电话', max_length = 20, default='', null=True)
-    #QQ
-    # QQnumber = models.CharField('QQ',max_length=13,default='',null=True,blank=True)
-
-
-
-    def __str__(self):
-        return self.username
-
-
 class User_Role(models.Model):
     # 角色名称
     rolename = models.CharField('名称', max_length = 30, default='')
@@ -51,3 +27,29 @@ class User_Grade(models.Model):
 
     def __str__(self):
         return self.gradename
+
+
+
+class Person(models.Model):
+    # 用户名
+    username = models.CharField('用户名', max_length=30, default='')
+    # 用户密码
+    password = models.CharField('密码', max_length=30, default='')
+    # 用户年龄
+    age = models.IntegerField('年龄', default=0, null=True,blank=True)
+    # 用户角色
+    role = models.ForeignKey('User_Role', on_delete=models.CASCADE, null=True, blank=True)
+    # 用户所处年级
+    grade = models.ForeignKey('User_Grade', on_delete=models.CASCADE, null=True, blank=True)
+    # 用户真实名称
+    name = models.CharField('真实姓名', max_length=30, default='', null=True)
+    # 联系电话
+    telephone = models.CharField('联系电话', max_length = 20, default='', null=True)
+    #QQ
+    # QQnumber = models.CharField('QQ',max_length=13,default='',null=True,blank=True)
+
+
+
+    def __str__(self):
+        return self.username
+
